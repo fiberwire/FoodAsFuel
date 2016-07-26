a435d269c9d7232354e94804c8d856bb5475dc1e
Andrew <andrew.ramos92@gmail.com>
2016-07-26 19:54:58 -0400

    created app

---
+++ app/components/containers/foodList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+//components
+import FoodItem from '../presentations/foodItem';
+
+//component to hold a list of FoodItems
+export default class FoodList extends Component {
+
+  render(){
+    return(
+      <View>
+        
+      </View>
+    )
+  }
+}
---
+++ app/models/food.js
@@ -0,0 +1,8 @@
+
+export default class Food{
+  constructor(price = 0, name = "", nutrition = {}){
+    this.price = price;
+    this.name = name;
+    this.nutrition = nutrition;
+  }
+}
